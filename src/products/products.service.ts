793c0d91b1ddbf49f612853590fa2ebba9100026
mufrick01 <mrmufrick@gmail.com>
2025-01-23 20:33:30 -0300

    GET & DELETE TypeORM [S10-132]

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -31,25 +31,37 @@ export class ProductsService {
 
   
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+       return await this.productRepository.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+      const product = await this.productRepository.findOneBy({id});
+      if(!product){
+        throw new NotFoundException(`product with id: [${id}] not found`)
+      }
+      return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    
+    await this.productRepository.delete(product.id);
+
+    return product;
+
   }
 
 
   private handleDbExceptions(error: any) {
     if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    if (error.code === '22P02') {
       throw new BadRequestException(error.detail);
     }
     this.logger.error(`[${error.code}] ${error.detail} => ${error}`);
