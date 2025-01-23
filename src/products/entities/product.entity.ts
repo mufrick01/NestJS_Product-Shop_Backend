eb4908f6b978dc3f0f07591e697cabbaf457f60a
mufrick01 <mrmufrick@gmail.com>
2025-01-23 19:50:39 -0300

    beforeInsert & beforeUpdate [S10-131]

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,45 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
+    
+    @Column('text',{unique:true})
+    title:string;
+
+    @Column('float',{default:0})
+    price:number;
+
+    @Column({type:'text', nullable:true})
+    description:string;
+
+    @Column('text',{unique:true})
+    slug:string;
+
+    @Column('int',{default:0})
+    stock:number;
+
+    @Column('text',{array:true})
+    sizes:string[];
+
+    @Column('text')
+    gender:string;
+
+    // TODO: tags
+    // TODO: images
+
+
+    @BeforeInsert()
+    checkSlotInsert(){
+        if(!this.slug){
+            this.slug = this.title
+        }
+        this.slug = this.slug.toLowerCase()
+                             .replaceAll(" ","_")
+                             .replaceAll("'","")
+    }
+
+    // @BeforeUpdate()
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,59 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository:Repository<Product>,
+  ){}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+     
+      const product = this.productRepository.create(createProductDto)
+      await this.productRepository.save(product);
+
+      return product;
+
+    } catch (error) {
+      
+      this.handleDbExceptions(error);
+    }
+  }
+
+  
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+
+
+  private handleDbExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    this.logger.error(`[${error.code}] ${error.detail} => ${error}`);
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+  }
+
+}
